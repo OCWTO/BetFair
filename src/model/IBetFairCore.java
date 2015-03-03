d8586ccd63246f59d553fb3c60d2491700c98933
Craig <rnb11172@CIS-BAIRD-02.ds.strath.ac.uk>
2015-03-03 12:13:15 +0000

    Added stub code and documentation to BetFair interface

--- src/model/IBetFairCore.java
+++ src/model/IBetFairCore.java
@@ -9,6 +9,7 @@ import java.util.List;
 import java.util.Set;
 
 import betFairGSONClasses.EventTypeResult;
+import betFairGSONClasses.LoginResponse;
 import betFairGSONClasses.MarketBook;
 import betFairGSONClasses.MarketCatalogue;
 import betFairGSONClasses.MarketFilter;
@@ -16,41 +17,111 @@ import betFairGSONClasses.PriceProjection;
 import enums.MarketProjection;
 import enums.MarketSort;
 import enums.OrderProjection;
+import exceptions.CryptoException;
 
 public interface IBetFairCore
 {
-	//cancelOrders
+	/**
+	 * NOT SUPPORTED
+	 */
+	//public CancelExecutionReport cancelOrders(String marketId, List<CancelInstruction> instructions, String customerRef);
 	
-	//listClearedOrders
+	/**
+	 * NOT SUPPORTED
+	 */
+	//public ClearedOrderSummaryReport listClearedOrders(BetStatus betStatus, Set<EventTypeId> eventTypeIds, Set<EventId> eventIds, Set<MarketId> marketIds, Set<RunnerId> runnerIds, Set<BetId> betIds, Side side, TimeRange settledDateRange, GroupBy groupBy, boolean includeItemDescription, String locale, int fromRecord, int recordCount);
 	
-	//listCompetitions
+	/**
+	 * NOT SUPPORTED
+	 */
+	//public List<CompetitionResult> listCompetitions(MarketFilter filter, String locale);
+	
+	/**
+	 * NOT SUPPORTED
+	 */
+	//public List<CountryCodeResult> listCountries(MarketFilter filter, String locale);
+	
+	/**
+	 * NOT SUPPORTED
+	 */
+	//public CurrentOrderSummaryReport listCurrentOrders(Set<String>betIds, Set<String> marketIds, OrderProjection orderProjection, TimeRange placedDateRange, OrderBy orderBy, SortDir sortDir, int fromRecord, int recordCount);
+	
+	/**
+	 * UPDATE JDOC
+	 * @param filter
+	 * @return
+	 */
+	public List<MarketCatalogue> listEvents(MarketFilter filter);
 
-	//listCountries
-	
-	//listCurrentOrders	
-	
-	//Return type here is different from API list but it's ok.
-	public List<MarketCatalogue> listEvents ( MarketFilter filter);
-
+	/**
+	 * UPDATE JDOC
+	 * @param filter
+	 * @return
+	 */
 	public List<EventTypeResult> listEventTypes(MarketFilter filter);
 	
-	public List<MarketBook> listMarketBook (List<String>marketIds , PriceProjection priceProjection, OrderProjection orderProjection);
+	/**
+	 * UPDATE JDOC
+	 * @param marketIds
+	 * @param priceProjection
+	 * @param orderProjection
+	 * @return
+	 */
+	public List<MarketBook> listMarketBook(List<String>marketIds, PriceProjection priceProjection, OrderProjection orderProjection);
 	
+	/**
+	 * UPDATE JDOC
+	 * @param filter
+	 * @param marketProjection
+	 * @param sort
+	 * @param maxResults
+	 * @param locale
+	 * @return
+	 */
 	public List<MarketCatalogue> listMarketCatalogue(MarketFilter filter, Set<MarketProjection>marketProjection, MarketSort sort, int maxResults, String locale);
 	
-	//listMarketProfitAndLoss
+	/**
+	 * NOT SUPPORTED
+	 */
+	//public List<MarketProfitAndLoss>	listMarketProfitAndLoss(Set<MarketId> marketIds, boolean includeSettledBets, boolean includeBspBets, boolean netOfCommission);
 
-	//listMarketTypes
+	/**
+	 * NOT SUPPORTED
+	 */
+	//public List<MarketTypeResult> listMarketTypes(MarketFilter filter ,String locale);
 	
-	//listVenues
+	/**
+	 * NOT SUPPORTED
+	 */
+	//public List<VenueResult> listVenues (MarketFilter filter ,String locale);
 
-	//login 
+	/**
+	 * 
+	 * @param username BetFair account username
+	 * @param password BetFair account password
+	 * @param filePassword BetFairs certificate file password
+	 * @return
+	 * @throws CryptoException if certificate file password is incorrect
+	 */
+	public LoginResponse login(String username, String password, String filePassword) throws CryptoException;
 	
-	//placeOrders
+	/**
+	 * NOT SUPPORTED
+	 */
+	//public PlaceExecutionReport placeOrders(String marketId, List<PlaceInstruction> instructions, String customerRef);
 	
-	//replaceOrders
+	/**
+	 * NOT SUPPORTED
+	 */
+	//public ReplaceExecutionReport replaceOrders(String marketId, List<ReplaceInstruction> instructions, String customerRef);
 	
-	//listTimeRanges
+	/**
+	 * NOT SUPPORTED
+	 */
+	//public List<TimeRangeResult> listTimeRanges(MarketFilter filter, TimeGranularity granularity);
 
-	//updateOrders
+	/**
+	 * NOT SUPPORTED
+	 */
+	//public UpdateExecutionReport updateOrders(StringmarketId, List<UpdateInstruction> instructions, String customerRef);
 }
