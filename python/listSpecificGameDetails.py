b1d8ec0e0251f92a293ad1f88071d2ec7893d865
Craig Thomson <0ocwto0@gmail.com>
2015-03-13 21:51:04 +0000

    Added methods tracking about of money matched in markets and another method for storing all json the program receives

--- python/listSpecificGameDetails.py
+++ python/listSpecificGameDetails.py
@@ -26,24 +26,25 @@ else:
 #login code above this
 print "--------------------------------"
 
-if success == True:
-	url="https://api.betfair.com/exchange/betting/json-rpc/v1"
-	header = { 'X-Application' : delayedKey, 'X-Authentication' : sessionToken ,'content-type' : 'application/json' }
-	
-	jsonrpc_req = '{"jsonrpc": "2.0","method": "SportsAPING/v1.0/listMarketCatalogue","params": {"filter": {"eventIds": ["27312440"]},"maxResults": "200","marketProjection": ["COMPETITION","EVENT","EVENT_TYPE","RUNNER_DESCRIPTION","RUNNER_METADATA","MARKET_START_TIME"]},"id": 1}'
+#
+#if success == True:
+#	url="https://api.betfair.com/exchange/betting/json-rpc/v1"
+#	header = { 'X-Application' : delayedKey, 'X-Authentication' : sessionToken ,'content-type' : 'application/json' }
+#	
+#	jsonrpc_req = '{"jsonrpc": "2.0","method": "SportsAPING/v1.0/listMarketCatalogue","params": {"filter": {"eventIds": ["27312440"]},"maxResults": "200","marketProjection": ["COMPETITION","EVENT","EVENT_TYPE","RUNNER_DESCRIPTION","RUNNER_METADATA","MARKET_START_TIME"]},"id": 1}'
  
-	response = requests.post(url, data=jsonrpc_req, headers=header)
+#	response = requests.post(url, data=jsonrpc_req, headers=header)
 	 
-	print json.dumps(json.loads(response.text), indent=3)
-else:
-	print "Second request failed."
+#	print json.dumps(json.loads(response.text), indent=3)
+#else:
+#	print "Second request failed."
 
 print "--------------------------------"
 
 if success == True:
 	newurl="https://api.betfair.com/exchange/betting/json-rpc/v1"
 	newheader = { 'X-Application' : liveKey, 'X-Authentication' : sessionToken ,'content-type' : 'application/json' }
-	newjsonrpc_req= '{"jsonrpc": "2.0","method": "SportsAPING/v1.0/listMarketBook","params": { "marketIds" : ["1.116564775"], "priceProjection":{"priceData":["EX_BEST_OFFERS"]}}, "id": 1}'
+	newjsonrpc_req= '{"jsonrpc": "2.0","method": "SportsAPING/v1.0/listMarketBook","params": { "eventIds" : ["27391856"], "priceProjection":{"priceData":["EX_BEST_OFFERS"]}}, "id": 1}'
 	newresponse = requests.post(newurl, data=newjsonrpc_req, headers=newheader)
 	print json.dumps(json.loads(newresponse.text), indent=3)
 else:
