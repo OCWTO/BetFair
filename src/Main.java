5a33b70f7105f77078eceb72db2a84b4072d1989
Craig Thomson <0ocwto0@gmail.com>
2015-03-31 23:03:33 +0100

    Added game logs

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+import views.TextFrontEnd;
 import model.ISimpleBetFair;
 import model.SimpleBetFair;
 
@@ -24,22 +25,22 @@ public class Main
 	//print out names are in home vs away, not too relevant but maybe display it in gui
 	public static void main(String[] args)
 	{
-		//TextFrontEnd textUi = new TextFrontEnd(false);
-		//textUi.start();
+		TextFrontEnd textUi = new TextFrontEnd(false);
+		textUi.start();
 		
 		
 		//TestFile x = new TestFile(new File("C:\\Users\\Craig\\Desktop\\Workspace\\BetFair\\logs\\gamelogs\\Netherlands v Turkey FULL\\rawjson.txt"));
 		//DataAnalysis x1 = new DataAnalysis(x);
 		//x1.start();
 		
-		ISimpleBetFair x = new SimpleBetFair(true);
-		
-		x.login("0ocwto0", "2014Project", "project", new File("C:\\Users\\Craig\\Desktop\\Workspace\\BetFair\\certs\\client-2048.p12"));
-		x.getSupportedSportList();
-		x.getGameListForSport("1");
-		x.getMarketsForGame("27408709");
-		List<String> xxx = new ArrayList<String>();
-		xxx.add("1.118007208");
-		x.getMarketInformation(xxx);
+//		ISimpleBetFair x = new SimpleBetFair(true);
+//		
+//		x.login("0ocwto0", "2014Project", "project", new File("C:\\Users\\Craig\\Desktop\\Workspace\\BetFair\\certs\\client-2048.p12"));
+//		x.getSupportedSportList();
+//		x.getGameListForSport("1");
+//		x.getMarketsForGame("27408709");
+//		List<String> xxx = new ArrayList<String>();
+//		xxx.add("1.118007208");
+//		x.getMarketInformation(xxx);
 	}
 }
